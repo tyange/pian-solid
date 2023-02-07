901cfc77ead38be5bb56eea355db4f8f908b2f9f
tyange <usun16@gmail.com>
2023-02-07 22:26:07 +0900

    header에 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,32 @@
+import { Link } from "@solidjs/router";
+
+import { IconUser, IconLogin, IconQuestionMark } from "@tabler/icons-solidjs";
 import logoImg from "../assets/red-x-on-pickle.png";
 
 export default function Header() {
   return (
     <header class="flex justify-center py-10">
-      <nav class="w-9/12 grid grid-cols-5 items-center">
+      <nav class="grid items-center w-5/12 grid-cols-5">
         <div class="w-20">
-          <img class="w-full" src={logoImg} alt="pian 로고" />
+          <Link href="/">
+            <img src={logoImg} alt="로고 이미지" />
+          </Link>
         </div>
         <div>
-          <ul class="flex gap-5">
-            <li>공지</li>
-            <li>버거들</li>
-          </ul>
+          <ul class="flex gap-5"></ul>
         </div>
-        <div class="col-span-3 justify-self-end mr-10">
-          <ul class="flex gap-3">
-            <li>github</li>
-            <li>QM</li>
+        <div class="col-span-3 mr-10 justify-self-end">
+          <ul class="flex gap-4">
+            <li>
+              <Link href="/auth">
+                <IconLogin stroke="2" size={30} />
+              </Link>
+            </li>
+            <li>
+              <Link href="/question">
+                <IconQuestionMark stroke="2" size={30} />
+              </Link>
+            </li>
           </ul>
         </div>
       </nav>
