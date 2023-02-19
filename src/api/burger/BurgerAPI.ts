0035e0d104f9cfee8a8d98127b0b333ab4f98b28
tyange <usun16@gmail.com>
2023-02-19 19:59:54 +0900

    버거 추가할 때 token을 헤더에 실어서 전송

--- src/api/burger/BurgerAPI.ts
+++ src/api/burger/BurgerAPI.ts
@@ -5,9 +5,16 @@ import { PageData } from "../../types/PageData";
 const BurgerAPI = {
   getAllBurger: async (pageParam: string) =>
     await AxiosInstance.get<{ Burgers: Burger[]; PageData: PageData }>(
-      `/burger?page=${pageParam ? pageParam : "0"}`,
-      { withCredentials: true }
+      `/burger?page=${pageParam ? pageParam : "0"}`
     ),
+  addBurger: async (newBurger: Burger, token: string) => {
+    await AxiosInstance.post("/burger", newBurger, {
+      headers: {
+        Authorization: token,
+      },
+      withCredentials: true,
+    });
+  },
 };
 
 export default BurgerAPI;
